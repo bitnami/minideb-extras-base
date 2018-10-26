fc41b30cc29c416a46aa066a4b3871855f519867
juan131 <juan@bitnami.com>
2018-10-26 15:04:46 +0200

    Refactor 'generic' Bash libraries

--- stretch/rootfs/liblog.sh
+++ stretch/rootfs/liblog.sh
@@ -1,32 +1,77 @@
 #!/bin/bash
+#
+# Library for logging functions
 
-# Color Palette
+# Constants
 RESET='\033[0m'
-BOLD='\033[1m'
-
-## Foreground
 RED='\033[38;5;1m'
 GREEN='\033[38;5;2m'
 YELLOW='\033[38;5;3m'
 MAGENTA='\033[38;5;5m'
 CYAN='\033[38;5;6m'
 
+# Functions
+
+########################
+# Prints to STDERR
+# Arguments:
+#   Message to print
+# Returns:
+#   None
+#########################
+stderr_print() {
+    printf "%b\n" "${*}" >&2
+}
+
+########################
+# Log message
+# Arguments:
+#   Message to log
+# Returns:
+#   None
+#########################
 log() {
     stderr_print "${NAMI_DEBUG:+${CYAN}${MODULE} ${MAGENTA}$(date "+%T.%2N ")}${RESET}${*}"
 }
-
+########################
+# Log an 'info' message
+# Arguments:
+#   Message to log
+# Returns:
+#   None
+#########################
 info() {
     log "${GREEN}INFO ${RESET} ==> ${*}"
 }
-
+########################
+# Log message
+# Arguments:
+#   Message to log
+# Returns:
+#   None
+#########################
 warn() {
     log "${YELLOW}WARN ${RESET} ==> ${*}"
 }
-
+########################
+# Log an 'error' message
+# Arguments:
+#   Message to log
+# Returns:
+#   None
+#########################
 error() {
     log "${RED}ERROR${RESET} ==> ${*}"
 }
-
-stderr_print() {
-    printf "%b\n" "${*}" >&2
+########################
+# Log a 'debug' message
+# Globals:
+#   BITNAMI_DEBUG
+# Arguments:
+#   None
+# Returns:
+#   None
+#########################
+debug() {
+    [[ "$BITNAMI_DEBUG" = true ]]  && log "${MAGENTA}DEBUG${RESET} ==> ${*}"
 }
